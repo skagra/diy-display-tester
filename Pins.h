5089848d26d7742132cd7d75e867f22fb5a584b4
Paul Harvey <Paul Harvey>
2023-03-23 13:11:45 +0000

    Tweaked docs

--- Pins.h
+++ Pins.h
@@ -1,9 +1,14 @@
 #ifndef _PINS_DOT_H_
 #define _PINS_DOT_H_
 
+// Interrupt pin
 #define PIN_INTERRUPT 2
+
+// Two ADDR pins defining the operation - LSB to HSB
 #define PIN_ADDR_LOW 3
 #define PIN_ADDR_HIGH 4
+
+// Eight data pins - LSB to HSB
 #define PIN_VAL_MIN 5
 #define PIN_VAL_MAX 12
 
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a simple test harness for memory mapped [display](https://github.com/ska
 # Interface
 
 The project uses an Arduino with connections defined in [Pins.h](Pins.h).
+
+In addition to these connections, the `ENABLE` line of the display circuit must be pulled high.
